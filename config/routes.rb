d2334218c03c8ad51b46797e0673fcb4c214b5c8
Muhamadjoni Rahimzod <mrahimzod@gmail.com>
2021-11-02 07:07:44 +0400

    Implemented CRUD actions for a to do list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #list all tasks
+  # root to: 'tasks#index'
+  # #list the details of a specific task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+  # #delete a task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  # #add a new task
+  # get '/task/new', to: 'tasks#new', as: :new_task
+  # post '/tasks', to: 'tasks#create'
+  # #edit a task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+  resources :tasks
 end
